c5e7f9b7ca86bc87f7c6149a49f21ddd4acd2785
Peter Akhigbe <akhigbe37@gmail.com>
2023-06-05 18:39:35 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,13 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT *
+FROM animals
+WHERE name LIKE '%mon'
+  OR (date_of_birth BETWEEN '2016-01-01' AND '2019-12-31')
+  OR (neutered = TRUE AND escape_attempts < 3)
+  OR name IN ('Agumon', 'Pikachu')
+  OR (weight_kg > 10.5)
+  OR (neutered = TRUE)
+  OR name != 'Gabumon'
+  OR (weight_kg BETWEEN 10.4 AND 17.3);
+
